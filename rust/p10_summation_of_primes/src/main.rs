433f627f9b9dfe880e9398b9495dbb063ce8c1e8
John <johnwilles@gmail.com>
2017-11-14 23:08:39 -0500

    Finish p10 and init p11

--- rust/p10_summation_of_primes/src/main.rs
+++ rust/p10_summation_of_primes/src/main.rs
@@ -1,27 +1,37 @@
 fn is_prime(num: u64) -> bool {
-  if num % 2 == 0 {
+
+  if num <= 1 {
     return false;
-  } else {
-    let mut i = 3;
-    while i < num {
-      if num % i == 0 {
-        return false;
-      }
-      i = i + 2;
-    }
+  }
+
+  if num <= 3 {
     return true;
+  }
+
+  if num % 2 == 0 || num % 3 == 0 {
+    return false;
+  }
+
+  let mut i = 5;
+  while i*i <= num {
+    if num % i == 0 || num % (i+2) == 0 {
+      return false;
+    }
+    i = i + 6;
   } 
+  
+  return true;
 }
 
 fn main() {
   
   let mut summation = 0u64;
 
-  for n in 2..2000001 {
+  for n in 0..2000001 {
     if is_prime(n) {
       summation = summation + n
     }  
   } 
 
-  println!("{}", summation + 1)
+  println!("{}", summation)
 }
