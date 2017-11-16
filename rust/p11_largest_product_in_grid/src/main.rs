80bed57b02c2f7cb715ef09d1270ff68a2b51f91
John <johnwilles@gmail.com>
2017-11-15 22:41:44 -0500

    Parse grid for p11

--- rust/p11_largest_product_in_grid/src/main.rs
+++ rust/p11_largest_product_in_grid/src/main.rs
@@ -1,3 +1,21 @@
+use std::io::{BufRead, BufReader};
+use std::fs::File;
+
 fn main() {
-    println!("Hello, world!");
+
+  let file = File::open("grid.txt").expect("cannot open file");
+  let file = BufReader::new(file);
+
+  let mut grid = Vec::new();
+
+  for line in file.lines().filter_map(|result| result.ok()) {
+    let res: Vec<i32> = line.split(" ").map(|s| s.parse::<i32>().unwrap()).collect();
+    grid.push(res);
+  }
+
+  println!("{:?}", grid);
 }
+
+
+
+
