ac2deb4bd2ce683fc21108afa99fe810e8643e77
John <johnwilles@gmail.com>
2017-11-17 18:46:38 -0500

    Start p12

--- rust/p12_highly_divisible_triangular_number/src/main.rs
+++ rust/p12_highly_divisible_triangular_number/src/main.rs
@@ -1,3 +1,20 @@
+
+fn gen_triangle() {
+
+}
+
+fn find_num_divisors() {
+
+}
+
 fn main() {
-    println!("Hello, world!");
+
+  let mut max_num_divisors = 0;
+  let mut i = 0;
+
+  while num_divisors < 500 {
+    let triangle = gen_triangle(i);
+    let num_divisors: i32 = find_num_divisors(triangle);    
+    i = i + 1
+  }
 }
