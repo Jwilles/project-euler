53b9324561fd588f32d1e4651f081d39538bf2da
John <johnwilles@gmail.com>
2017-11-18 10:25:06 -0500

    Finish p12

--- rust/p12_highly_divisible_triangular_number/src/main.rs
+++ rust/p12_highly_divisible_triangular_number/src/main.rs
@@ -1,20 +1,41 @@
 
-fn gen_triangle() {
-
+fn gen_triangle(i: i32) -> i32 {
+  return (i * (i+1)) / 2;
 }
 
-fn find_num_divisors() {
+fn find_num_divisors(triangle: i32) -> i32 {
 
+  let mut num_divisors = 0;
+  
+
+  for i in 1..((triangle as f64).sqrt() as i32) {
+    if triangle % i == 0 {
+      if triangle / i == i {
+        num_divisors = num_divisors + 1;
+      } else {
+        num_divisors = num_divisors + 2;
+      }
+    }
+  }
+ 
+  return num_divisors;
 }
 
 fn main() {
 
   let mut max_num_divisors = 0;
   let mut i = 0;
+  let mut triangle = 0;
 
-  while num_divisors < 500 {
-    let triangle = gen_triangle(i);
+  while max_num_divisors < 500 {
+    triangle = gen_triangle(i);
     let num_divisors: i32 = find_num_divisors(triangle);    
-    i = i + 1
+    if num_divisors > max_num_divisors {
+      max_num_divisors = num_divisors;
+    }
+    i = i + 1;
   }
+
+  println!("{}", triangle);
+  println!("{}", max_num_divisors);
 }
