684340bdb3496a18e3960d706e7869ba2591c53c
John <johnwilles@gmail.com>
2017-11-19 20:41:33 -0500

    working big int

--- rust/p13_large_sum/src/main.rs
+++ rust/p13_large_sum/src/main.rs
@@ -1,3 +1,46 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{BufRead, BufReader};
+use std::fs::File;
+
+extern crate num;
+use num::bigint::BigInt;
+use num::bigint::Sign;
+
+
+fn read_numbers(filename: &str) -> Vec<BigInt> {
+
+  let mut numbers = Vec::new();
+
+  let file = File::open(filename).expect("cannot open file");
+  let file = BufReader::new(file);
+
+//  println!("{:?}", Sign::Plus);
+
+  for line in file.lines().filter_map(|result| result.ok()) {
+
+    let chars = line.chars();
+    let mut res = Vec::new();
+    
+    for char in chars {
+     res.push(char.to_string().parse().unwrap());
+    }
+     
+//    println!("{}", line);
+//    let res: Vec<u32> = line.split(' ').map(|s| s.parse().unwrap()).collect();
+ //   println!("{:?}", res);
+//    numbers.push(res);    
+    numbers.push(BigInt::new(Sign::Plus, res));
+  }
+
+  return numbers;
 }
+
+
+fn main() {
+
+  let filename = "numbers.txt";
+  let numbers = read_numbers(filename);
+
+  println!("{:?}", numbers);
+
+}
+
