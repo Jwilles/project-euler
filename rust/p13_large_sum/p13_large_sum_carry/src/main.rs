cd6adacda05003a9afb3c4a3024d4dfb4e76cc5b
John <johnwilles@gmail.com>
2017-11-20 20:10:46 -0500

    Finish p13

--- rust/p13_large_sum/p13_large_sum_carry/src/main.rs
+++ rust/p13_large_sum/p13_large_sum_carry/src/main.rs
@@ -1,34 +1,17 @@
 use std::io::{BufRead, BufReader};
 use std::fs::File;
 
-extern crate num;
-use num::bigint::BigInt;
-use num::bigint::Sign;
 
-
-fn read_numbers(filename: &str) -> Vec<BigInt> {
+fn read_numbers(filename: &str) -> Vec<Vec<u32>> {
 
   let mut numbers = Vec::new();
 
   let file = File::open(filename).expect("cannot open file");
   let file = BufReader::new(file);
 
-//  println!("{:?}", Sign::Plus);
-
   for line in file.lines().filter_map(|result| result.ok()) {
-
-    let chars = line.chars();
-    let mut res = Vec::new();
-    
-    for char in chars {
-     res.push(char.to_string().parse().unwrap());
-    }
-     
-//    println!("{}", line);
-//    let res: Vec<u32> = line.split(' ').map(|s| s.parse().unwrap()).collect();
- //   println!("{:?}", res);
-//    numbers.push(res);    
-    numbers.push(BigInt::new(Sign::Plus, res));
+    let res: Vec<u32> = line.chars().map(|s| s.to_string().parse().unwrap()).collect();
+    numbers.push(res);    
   }
 
   return numbers;
@@ -40,17 +23,29 @@ fn main() {
   let filename = "numbers.txt";
   let numbers = read_numbers(filename);
 
-  let mut sum_total = BigInt::new(Sign::Plus, vec![0]); 
-
-  for number in numbers {
-    sum_total = sum_total + number;   
-    println!("{:?}", sum_total);
-  // sum_total = sum_total::Add(number); 
-  }
+  let mut sum_total = Vec::new(); 
+  let num_cols = numbers[0].len();
+  let num_rows = numbers.len();
+  let mut carry = 0;
 
 
-//  println!("{:?}", numbers);
-//  println!("{}", sum_total::to_u64);
+  for i in (0..(num_cols)).rev() {
 
+    let mut sum_col = carry;
+
+    for j in 0..num_rows {
+      sum_col = sum_col + numbers[j][i];      
+    } 
+    if i == 0 {
+      sum_total.push(sum_col)
+    } else {
+      sum_total.push(sum_col % 10);
+    }
+
+    carry = sum_col / 10; 
+}
+  
+
+  println!("{:?}", sum_total);
 }
 
