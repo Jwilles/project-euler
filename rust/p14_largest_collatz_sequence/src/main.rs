481d33ebe8bba435b30908520301e2792455eef8
John <johnwilles@gmail.com>
2017-11-21 21:15:46 -0500

    Finish p14

--- rust/p14_largest_collatz_sequence/src/main.rs
+++ rust/p14_largest_collatz_sequence/src/main.rs
@@ -1,3 +1,27 @@
+
+
 fn main() {
-    println!("Hello, world!");
+
+  let mut max_count = 0;
+  let mut max_num = 0;
+
+  for i in 1..1000001 {
+    let mut n: u64 = i;
+    let mut count = 0;
+    while n != 1 {
+      count = count + 1;
+      if n % 2 == 0 {
+        n = n / 2; 
+      } else {
+        n = (3 * n) + 1;   
+      }
+    }
+    if max_count < count {
+      max_count = count;
+      max_num = i;
+    }
+  }
+  println!("{}", max_count);
+  println!("{}", max_num);
+
 }
