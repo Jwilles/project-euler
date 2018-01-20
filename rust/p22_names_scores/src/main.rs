e57a9886d486eb3479e70714128512ab1d3bbb91
John <johnwilles@gmail.com>
2018-01-19 23:21:23 -0500

    Add p22 name parsing

--- rust/p22_names_scores/src/main.rs
+++ rust/p22_names_scores/src/main.rs
@@ -1,3 +1,24 @@
+use std::io::{BufRead, BufReader};
+use std::fs::File;
+
+fn read_names(filename: &str) -> Vec<&str> {
+
+  let file = File::open(filename).expect("cannot open file");
+  let file = BufReader::new(file);
+
+  let mut vec = Vec::new();
+
+  for line in file.lines().filter_map(|result| result.ok()) {
+    let mut res = line.split(",");
+    vec = res.collect::<Vec<&str>>();
+  }
+
+  return vec;
+}
+
 fn main() {
-    println!("Hello, world!");
+  let filename = "names.txt";
+  let names = read_names(filename);
+
+  println!("{:?}", names);
 }
